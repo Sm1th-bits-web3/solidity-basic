abc455c33aba457608a72feeafaf9309196e7e75
Sm1th-bits-web3 <sm1th.bits.web3@gmail.com>
2024-07-25 15:29:32 +0800

    Update flow.sol

--- flow.sol
+++ flow.sol
@@ -1,9 +1,10 @@
 // SPDX-License-Identifier: MIT
+//if function do not have 'view' or 'public',the function will spend gas
 pragma solidity ^0.4.0;
 contract flow{
     uint8 num1 = 255;
     uint8 num2 = 0;
-    function flow1() returns (uint){
+    function flow1() view public returns (uint){
         return num1+1;
     }
     function flow2() returns (uint){
